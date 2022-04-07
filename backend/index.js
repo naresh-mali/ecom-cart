0bc1e9f1da34faeffe13e1be31e25ac456eaddb4
naresh-mali <nareshm.techno@gmail.com>
2022-04-07 21:22:48 +0530

    nodemon added

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
-const express= require('express');
-
-
-const app = express()
-
-app.get("/",(req,res)=>{
-    res.send("App is working..")
-});
-
-app.listen(5000)
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const connectDB = async () => {
+  mongoose.connect("mongodb://localhost:27017/e-comm");
+  const productSchema=new mongoose.Schema({})
+  const product=mongoose.model('product',productSchema);
+  const data = await product.find();
+  console.warn(data); 
+};
+connectDB();
+app.listen(5000);
