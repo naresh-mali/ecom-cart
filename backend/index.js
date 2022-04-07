eb071b28161352c36b844192284a1a7bbb2cc60d
naresh-mali <nareshm.techno@gmail.com>
2022-04-07 22:54:59 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 require('./db/config');
 const User = require("./db/User");
 const app = express();
+app.use(express.json());
 
-app.post("/register",(req,resp)=>{
- resp.send("api in progress..")
+app.post("/register", async(req,resp)=>{
+    let user=new User(req.body);
+    let result = await user.save();
+ resp.send(result);
 })
 
 app.listen(5000);
